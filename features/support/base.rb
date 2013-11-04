4eeba1731a8cef3efaf54d9fe407def4c08b22de
Joseph Shraibman <jshraibman@mdsol.com>
2013-11-04 18:49:59 -0500

    Defend against nil

--- features/support/base.rb
+++ features/support/base.rb
@@ -3,5 +3,5 @@
 require 'aruba/cucumber'
 
 After do
-  FileUtils.rm_rf  @current_dir if File.directory?(@current_dir)
+  FileUtils.rm_rf @current_dir if @current_dir && File.directory?(@current_dir)
 end
